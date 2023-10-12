8761443296a883737ca44007b6ffae804b5d8bef
Malvere <gatling1404@gmail.com>
2023-10-12 23:55:20 +0300

    edit

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from time import sleep
 
-from web.driver import Captcha, PrigozhinSelenium
+from web.driver import PrigozhinSelenium
 
 bot = PrigozhinSelenium()
 bot.raw_go()
 try:
-    cpt = bot.captcha()
-    print("First try")
+    bot.parse()
 except:
-    sleep(30)
-    cpt = bot.captcha()
-    print("Ready! 2nd try")
+    try:
+        cpt = bot.captcha()
+        print("First try")
+    except:
+        sleep(30)
+        cpt = bot.captcha()
+        print("Ready! 2nd try")
